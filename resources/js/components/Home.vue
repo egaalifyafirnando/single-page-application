b669c4351b1a7a303537356a48951b011deaf9f7
Ega Alifya Firnando <egaalifyaf@gmail.com>
2021-06-16 00:22:41 +0700

    first commit

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="jumbotron">
+        <h1 class="display-4">Selamat Datang</h1>
+        <p class="lead">Belajar Laravel Vue SPA - Single Page Application</p>
+        <hr class="my-4" />
+        <router-link
+            :to="{ name: 'about' }"
+            class="btn btn-primary btn-lg"
+            role="button"
+            >SELENGKAPNYA</router-link
+        >
+    </div>
+</template>
